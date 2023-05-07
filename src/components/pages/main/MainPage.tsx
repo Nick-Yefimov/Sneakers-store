c1b22a409e280e96d3d06c1108b5bb3860a6b363
shinigamiii11 <kometmontana22@gmail.com>
2023-05-07 12:58:16 +0300

    add header component in cart page

--- src/components/pages/main/MainPage.tsx
+++ src/components/pages/main/MainPage.tsx
@@ -1,10 +1,12 @@
 import AppHeader from '../../appHeader/AppHeader';
 import AppProducts from '../../appProducts/AppProducts';
+import AppSearch from '../../appSearch/AppSearch';
 
 const MainPage: React.FC = () => {
     return (
         <div className='wrapper'>
             <AppHeader />
+            <AppSearch />
             <AppProducts />
         </div>
     )
